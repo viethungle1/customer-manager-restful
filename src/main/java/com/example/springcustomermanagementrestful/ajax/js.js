d027fe7283b2ebd83eea4a51d378cd9c38e60208
viethungle1 <hunglv0801@gmail.com>
2024-03-12 16:25:44 +0700

    crud AJAX web-service

---
+++ src/main/java/com/example/springcustomermanagementrestful/ajax/js.js
@@ -0,0 +1,38 @@
+function showAllCustomer() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/customer",
+        success: function (data) {
+            console.log(data)
+            let content = "";
+            for (let i = 0; i < data.length; i++) {
+                content +=`
+            <tr>
+            <td>${data[i].id}</td>
+            <td>${data[i].firstName}</td>
+            <td>${data[i].lastName}</td>
+            </tr>`
+            }
+            document.getElementById("list").innerHTML=content;
+        },})
+}
+showAllCustomer();
+function createNewCustomer() {
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let newCustomer= {
+        "firstName": firstName,
+        "lastName": lastName
+    }
+    $.ajax({
+        headers :{
+            'Accept': 'Application/json',
+            'Content-Type': 'Application/json'
+        },
+        type: "POST",
+        url: "http://localhost:8080/api/customer",
+        data: JSON.stringify(newCustomer),
+        success: showAllCustomer
+    })
+}
+
